4e1c0faefd243f1e0afd23880e1487c46b3c487a
ikeo <ikeomajesty@gmail.com>
2020-09-20 14:31:08 +0200

    small update to standing change  matrix

--- src/Standings.js
+++ src/Standings.js
@@ -10,7 +10,7 @@ function Standings(list) {
     factionsLeft.shift()
     // console.log("2",factionsLeft);
       factionsLeft.forEach(y => {
-          let standing = randomiser([-3, -2, -2, -1, -1, -1, 0, 0, 0, 0 , 1, 1, 1, 2, 2, 3])
+          let standing = randomiser([-3, -2, -2, -2, -1, -1, -1, -1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 3])
           standings.push({a: x.name, b: y.name, value: standing })
         //   console.log("Standings update:",standings);
       })
