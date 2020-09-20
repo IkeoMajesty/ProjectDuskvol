92f1129483aab2c12a7647d3dd95722ffbb7952a
ikeo <ikeomajesty@gmail.com>
2020-09-20 18:56:11 +0200

    Added polish factions

--- src/Factions.js
+++ src/Factions.js
@@ -1,23 +1,247 @@
-const factions = [{
-    name: "Johns",
-    group: "underworld"
-},
-{
-    name: "Bobs",
-    group: "goverment"
-},
-{
-    name: "Sailors",
-    group: "labor"
-},
-{
-    name: "Teachers",
-    group: "academics"
-}
+const factions = [
+    {
+        name: "Rój",
+        group: "polswiatek"
+    },
+    {
+        name: "Niewidoczni",
+        group: "polswiatek"
+    },
+    {
+        name: "Krąg Ognia",
+        group: "polswiatek"
+    },
+    {
+        name: "Lord Scurlock",
+        group: "polswiatek"
+    },
+    {
+        name: "Srebrne Ćwieki",
+        group: "polswiatek"
+    },
+    {
+        name: "Czerwone Szarfy",
+        group: "polswiatek"
+    },
+    {
+        name: "Siekacze",
+        group: "polswiatek"
+    },
+    {
+        name: "Siostry Ściemne",
+        group: "polswiatek"
+    },
+    {
+        name: "Szare Płaszcze",
+        group: "polswiatek"
+    },
+    {
+        name: "Szlifierze",
+        group: "polswiatek"
+    },
+    {
+        name: "Upiory",
+        group: "polswiatek"
+    },
+    {
+        name: "Wrony",
+        group: "polswiatek"
+    },
+    {
+        name: "Zaciemniacze",
+        group: "polswiatek"
+    },
+    {
+        name: "Ogary Mgieł",
+        group: "polswiatek"
+    },
+    {
+        name: "Pancerny Ulf",
+        group: "polswiatek"
+    },
+    {
+        name: "Zagubieni",
+        group: "polswiatek"
+    },
+    {
+        name: "Siły Zbrojne Imperium",
+        group: "instytucje"
+    },
+    {
+        name: "Poławiacze Lewiatanów",
+        group: "instytucje"
+    },
+    {
+        name: "Ministerstwo Ochrony",
+        group: "instytucje"
+    },
+    {
+        name: "Rada Miejska",
+        group: "instytucje"
+    },
+    {
+        name: "Iskrownicy",
+        group: "instytucje"
+    },
+    {
+        name: "Strażnicy Dusz",
+        group: "instytucje"
+    },
+    {
+        name: "Więzienie Żelazny Hak",
+        group: "instytucje"
+    },
+    {
+        name: "Aspiranci",
+        group: "instytucje"
+    },
+    {
+        name: "Błękitne Płaszcze",
+        group: "instytucje"
+    },
+    {
+        name: "Konsulat Iruwii",
+        group: "instytucje"
+    },
+    {
+        name: "Konsulat Skowlanu",
+        group: "instytucje"
+    },
+    {
+        name: "Brygada",
+        group: "instytucje"
+    },
+    {
+        name: "Konsulat Seweros",
+        group: "instytucje"
+    },
+    {
+        name: "Konsulat Wysp Sztyletowych",
+        group: "instytucje"
+    },
+    {
+        name: "Fundamentaliści",
+        group: "pracownicy"
+    },
+    {
+        name: "Dokerzy",
+        group: "pracownicy"
+    },
+    {
+        name: "Gondolierzy",
+        group: "pracownicy"
+    },
+    {
+        name: "Marynarze",
+        group: "pracownicy"
+    },
+    {
+        name: "Robotnicy",
+        group: "pracownicy"
+    },
+    {
+        name: "Dorożkarze",
+        group: "pracownicy"
+    },
+    {
+        name: "Kałamarzyści",
+        group: "pracownicy"
+    },
+    {
+        name: "Szyfranci",
+        group: "pracownicy"
+    },
+    {
+        name: "Majchry Kolejowe",
+        group: "pracownicy"
+    },
+    {
+        name: "Służący",
+        group: "pracownicy"
+    },
+    {
+        name: "Biała Korona",
+        group: "mieszczanie"
+    },
+    {
+        name: "Jasnomurze",
+        group: "mieszczanie"
+    },
+    {
+        name: "Pierwomurze",
+        group: "mieszczanie"
+    },
+    {
+        name: "Sześć Wież",
+        group: "mieszczanie"
+    },
+    {
+        name: "Bigowy Wądół",
+        group: "mieszczanie"
+    },
+    {
+        name: "Doki",
+        group: "mieszczanie"
+    },
+    {
+        name: "Jedwabny Brzeg",
+        group: "mieszczanie"
+    },
+    {
+        name: "Nocny Targ",
+        group: "mieszczanie"
+    },
+    {
+        name: "Węglowa Grań",
+        group: "mieszczanie"
+    },
+    {
+        name: "Wronia Stopa",
+        group: "mieszczanie"
+    },
+    {
+        name: "Burobagnie",
+        group: "mieszczanie"
+    },
+    {
+        name: "Charłakowa Jama",
+        group: "mieszczanie"
+    },
+    {
+        name: "Kościół Cielesnej Ekstazy",
+        group: "odszczepieńcy"
+    },
+    {
+        name: "Horda",
+        group: "odszczepieńcy"
+    },
+    {
+        name: "Pojednani",
+        group: "odszczepieńcy"
+    },
+    {
+        name: "Ścieżka Szeptów",
+        group: "odszczepieńcy"
+    },
+    {
+        name: "Skowlańscy Uchodźcy",
+        group: "odszczepieńcy"
+    },
+    {
+        name: "Zapomniane Bóstwa",
+        group: "odszczepieńcy"
+    },
+    {
+        name: "Łupieżcy Martwych Ziem",
+        group: "odszczepieńcy"
+    },
+    {
+        name: "Płacząca Pani",
+        group: "odszczepieńcy"
+    },
+    
 ]
 
 export default factions; 
-
-
 
 
